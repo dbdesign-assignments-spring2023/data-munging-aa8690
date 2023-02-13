62edf5ff9849e41c06b19e935f4c6bbc8d5047f2
aa8690 <aa8690@nyu.edu>
2023-02-13 16:26:39 -0500

    i finished!

--- analyze.py
+++ analyze.py
@@ -1,1 +1,35 @@
-# Place code below to do the analysis part of the assignment.
+# Afsana Ahmed, Analyze NASA Global Temperate Index Data
+
+import csv
+
+c = open('data/clean_data.csv','r')
+
+reader = csv.reader(c)
+
+all_data = c.read()
+all_data = all_data.split("\n")
+
+for i in range(len(all_data)):
+    all_data[i] = all_data[i].split(",")
+
+count = 1
+decade_mean = []
+temp = []
+decades_list = ['1880 - 1889:', '1890 - 1899:', '1900 - 1909:', '1910 - 1919:', '1920 - 1929:', '1930 - 1939:', '1940 - 1949:', '1950 - 1959:', '1960 - 1969:', '1970 - 1979:', '1980 - 1989:', '1990 - 1999:', '2000 - 2009:', '2010 - 2019:','2020 - Present:']
+
+for i in range(1, len(all_data)):
+    if count == 10:
+        count = 1
+        avg_temp = format(((sum(temp)) / 10),'.1f')
+        decade_mean.append(avg_temp)
+        temp = []
+
+    if all_data[i][13][0] != "*":
+        temp.append(float(all_data[i][13])),
+    
+    count += 1
+
+print("Average Temperature Anomaly in Degrees Farenheit For Each Decade (1880 - Present)")
+
+for v in range(len(decade_mean)):
+    print(decades_list[v], decade_mean[v], "degrees Fahrenheit")
--- munge.py
+++ munge.py
@@ -1,1 +1,46 @@
-# Place code below to do the munging part of this assignment.
+# Afsana Ahmed, Munge NASA Index Data
+
+resultdata = []
+
+o = open('data/nasa_index.txt','r')
+data = o.read()
+data = data.split("\n")
+
+for element in data:
+    if element == '':
+        data.remove(element)
+
+data = data[5:156]
+months = 'Year   Jan  Feb  Mar  Apr  May  Jun  Jul  Aug  Sep  Oct  Nov  Dec    J-D D-N    DJF  MAM  JJA  SON  Year'
+
+for element in data:
+    if element == months:
+        data.remove(element)
+
+data.insert(0,months)
+
+c = open('data/clean_data.csv','w')
+
+for i in range(len(data)):
+    data[i] = data[i].split()
+
+for i in range(1,len(data)):
+    for j in range(1,len(data[i])-1):
+        element = data[i][j]
+
+        if element[0] != "*":
+            element = int(element)
+            data[i][j] = (element / 100) * 1.8
+            data[i][j] = (format(data[i][j],'.1f'))
+
+    # write each piece of data to the list and include line breaks
+
+for i in data:
+  resultdata.append(','.join(i))
+
+resultdata = '\n'.join(resultdata)
+
+c.write(resultdata)
+# print(resultdata)
+
+c.close()
